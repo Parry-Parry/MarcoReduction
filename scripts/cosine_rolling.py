401d311f95a5cad2a82137aa756c68d58621ae13
Andrew Parry <2393265p@student.gla.ac.uk>
2022-12-06 16:34:19 +0000

    Testing cosine rolling

--- scripts/cosine_rolling.py
+++ scripts/cosine_rolling.py
@@ -6,14 +6,17 @@ from typing import NamedTuple, Any
 import faiss 
 import numpy as np
 import logging 
+from functools import partial
 import multiprocessing as mp
 
 from queryreduce.utils.utils import time_output
 
-class CentroidConfig(NamedTuple):
+class AcceptanceConfig(NamedTuple):
     states : Any
+    metric : str 
     sub : int 
     alpha : float 
+    update : int 
     compare : str 
     gpus : int
 
@@ -45,14 +48,29 @@ k : int -> Desired number of samples
 '''
 
 class Sampler:
-    def __init__(self, config : CentroidConfig) -> None:
+    def __init__(self, config : AcceptanceConfig) -> None:
+        compare = {
+            'max' : self._compare_max,
+            'mean' : self._compare_mean
+        }
+
         self.states = config.states
         faiss.normalize_L2(self.states)
         self.id = np.arange(len(config.states), dtype=np.int64)
         self.sub = config.sub
-        self.alpha = config.alpha
+        self.update = config.update
 
         self.idx = []
+        self.centroid = np.zeros((1, config.states.shape[-1]), dtype=np.int64)
+        self.compare = compare[config.compare]
+        
+        self.distance = np.inner
+    
+    def _compare_max(self, x, c) -> float:
+        return np.max(x) < np.max(c)
+
+    def _compare_mean(self, x, c) -> float:
+        return np.mean(x) < np.mean(c)
 
     def _threshold(self, x):
         '''
@@ -66,24 +84,37 @@ class Sampler:
             indices = np.random.choice(self.idx, self.sub, replace=False)
         elif len(self.idx) == 0:
             logging.debug('No Candidates')
-            return -1.
+            return -1
         else:
             logging.debug('Less candidates than subset')
             indices = self.idx
-        centroid = np.mean(self.states[indices], axis=0)
-        return np.inner(x, centroid)
+        
+        vecs = self.states[indices]
+        dist_x = self.distance(x, vecs)
+        dist_c = self.distance(self.centroid, vecs)
 
-    def run(self, k) -> np.array:
+        return self.compare(dist_x, dist_c)
+
+    def run(self, x0, k) -> np.array:
+        faiss.omp_set_num_threads(mp.cpu_count())
+        x_init = self.states[x0]
+        self.centroid = np.expand_dims(x_init, axis=0)
+        ticker = 0 # Update Ticker
         t = 0 # Total Steps
-        logging.info(f'Retrieving {k} candidates...')
+        logging.info(f'Retrieving {k} candidates with starting id: {x0}')
         start = time.time()
         while len(self.idx) < k:
             x_cand = np.random.choice(self.id)
             np.delete(self.id, x_cand)
             threshold = self._threshold(np.expand_dims(self.states[x_cand], axis=0))
             logging.debug(f'Threshold value {threshold}')
-            if threshold < self.alpha:
+            if threshold:
+                ticker += 1
                 self.idx.append(x_cand)
+                if ticker % self.update == 0:
+                    logging.debug(f'Updating Centroid at step {t}')
+                    self.centroid = np.expand_dims(np.mean(self.states[self.idx], axis=0), axis=0)
+                
             if t % 1000 == 0:
                 diff = time.time() - start
                 logging.info(f'Time Elapsed over {t} steps: {diff} | {len(self.idx)} candidates found')
@@ -97,7 +128,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-source', type=str)
 parser.add_argument('-sub', type=int, default=10)
-parser.add_argument('-alpha', type=float, default=1.)
+parser.add_argument('-update', type=int, default=100)
 parser.add_argument('-compare', type=str, default='mean')
 parser.add_argument('-samples', type=int, default=1)
 parser.add_argument('-out', type=str, default='/')
@@ -116,9 +147,9 @@ def main(args):
    
     config = AcceptanceConfig(
         states=array,
-        metric=args.metric,
+        metric=None,
         sub=args.sub,
-        alpha=args.alpha,
+        alpha=None,
         update=args.update,
         compare=args.compare,
         gpus = args.ngpu
@@ -143,20 +174,5 @@ if __name__ == '__main__':
     if args.verbose: log_level = logging.DEBUG
     else: log_level = logging.INFO
     logging.basicConfig(format='%(asctime)s - %(message)s', level=log_level)
-    logging.info('--Initialising Candidate Choice Using Acceptance Threshold Cosine Similarity Sampler--')
+    logging.info('--Initialising Candidate Choice Using Acceptance Threshold Sampler--')
     main(args)
-
-
-
-            
-            
-            
-
-
-
-
-
-
-
-
-
