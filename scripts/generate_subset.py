e2362117e4ee255045f2534aa7fd92441f53ca75
Andrew Parry <2393265p@student.gla.ac.uk>
2022-12-06 15:58:49 +0000

    Adding subset gen

--- scripts/generate_subset.py
+++ scripts/generate_subset.py
@@ -3,35 +3,24 @@ from math import trunc
 import pandas as pd
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Build Subset of IR Dataset in the form of <qid, docid_a, docid_b>')
+parser = argparse.ArgumentParser(description='Generate subsets of any array')
 
 parser.add_argument('-source', type=str, default=None, help='Dataset Source')
-parser.add_argument('-type', type=str, default=None, help='Source Type')
 parser.add_argument('-portion', type=int, help=r'% of dataset to use')
+parser.add_argument('-dupes', type=int, help='How many random subsets')
 parser.add_argument('-suffix', type=str, default='train', help='Suffix of output name')
 parser.add_argument('-out', type=str, help='Output dir for tsv')
 
-
 def main(args):
-    if args.type == 'tsv':
-        cols = ['qid', 'pid+', 'pid-']
-        types = {col : str for col in cols}
-        with open(args.source, 'r') as f:
-            df = pd.read_csv(f, sep='\t', header=None, index_col=False, names=cols, dtype=types)
-    else:
-        assert args.type == 'jsonl'
-        with open(args.source, 'r') as f:
-            df = pd.read_json(f, lines=True, orient='records')
+    cols = ['qid', 'pid+', 'pid-']
+    types = {col : str for col in cols}
+   
+    df = pd.read_csv(args.source, sep='\t', header=None, index_col=False, names=cols, dtype=types)
+    num_choices = trunc(len(df) * (args.portion/100))
 
     for i in range(args.dupes):
-        if not args.k:
-            num_choices = trunc(len(df) * (args.portion/100))
-        else:
-            num_choices = args.k
         sub_df = df.sample(n=num_choices)
-
-        string = embed.{args.suffix}.{i}.npy
-
+        sub_df.to_csv(args.out + f'triples.{args.suffix}.{i}.tsv', sep='\t', index=False, header=False)
 
 if __name__ == '__main__':
     main(parser.parse_args())
