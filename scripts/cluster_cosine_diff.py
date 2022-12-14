f5ccd385bdceb5be30ac1bc8270c4856cbfee319
Andrew Parry <2393265p@student.gla.ac.uk>
2022-12-14 11:46:39 +0000

    Added metropolis hastings algo

---
+++ scripts/cluster_cosine_diff.py
@@ -0,0 +1,132 @@
+import re
+import time
+import pyterrier as pt
+pt.init()
+
+import argparse
+import logging
+from typing import NamedTuple
+import multiprocessing as mp
+from math import ceil, floor
+
+import numpy as np
+import pandas as pd
+import faiss 
+
+class ClusterConfig(NamedTuple):
+    niter : int
+    nclust : int 
+    cmin : int 
+
+class ClusterEngine:
+    def __init__(self, config) -> None:
+        self.niter = config.niter
+        self.nclust = config.nclust
+        self.min = config.cmin 
+        self.kmeans = None
+    
+    def query(self, x) -> np.array:
+        assert self.kmeans is not None
+        _, I = self.kmeans.index.search(x, 1)
+        return I.ravel()
+
+    def train(self, x) -> None:
+        self.kmeans = faiss.Kmeans(x.shape[-1], self.nclust, niter=self.niter, verbose=False, spherical=True, min_points_per_centroid=self.min)
+        self.kmeans.train(x)
+
+def cosine_scoring(array):
+    q = array[:, :args.dim]
+    p1 = array[:, args.dim:2*args.dim]
+    p2 = array[:, 2*args.dim:3*args.dim]
+
+    p1_sim = np.vectorize(lambda x: )
+
+    return np.dot(q, p1) - np.dot(q, p2)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-textsource', type=str)
+parser.add_argument('-embedsource', type=str)
+parser.add_argument('-niter', type=int)
+parser.add_argument('-nclust', type=int)
+parser.add_argument('-dim', type=int)
+parser.add_argument('-candidates', type=int)
+parser.add_argument('-out', type=str)
+
+parser.add_argument('--index', type=str)
+parser.add_argument('--verbose', action='store_true')
+
+def main(args):
+    faiss.omp_set_num_threads(mp.cpu_count())
+
+    cols = ['query', 'psg+', 'psg-']
+    types = {col : str for col in cols}
+    logging.info('Reading Text...')
+    
+    triples_df = pd.read_csv(args.textsource, sep='\t', header=None, index_col=False, names=cols, dtype=types)
+
+    logging.info('Reading Embeddings...')
+    with open(args.embedsource, 'rb') as f:
+        array = np.load(f)
+
+    per_cluster = args.candidates // args.nclust
+
+    config = ClusterConfig(
+        niter=args.niter,
+        nclust=args.nclust,
+        cmin=per_cluster
+    )
+
+    start = time.time()
+
+    logging.info('Clustering Embeddings')
+    clustering = ClusterEngine(config)
+    clustering.train(array)
+    c_idx = clustering.query(array)
+    index = np.arange(len(array))
+
+    counts = np.unique(c_idx)
+
+    scale = counts.median()
+    diff = floor(scale / per_cluster)
+    print(f'Adding {diff} extra samples when over median: {scale} and max {counts.max()}')
+    
+    idx =[]
+    logging.info('In Centroid Ranking with Cosine Similarity...')
+
+    for i in range(args.nclust):
+        tmp_array = array[np.where(c_idx==i)]
+        scoring = None
+        tmp_idx = index[scoring]
+        if len(tmp_array) >= per_cluster:
+            if counts[i] > scale: candidates = tmp_idx[:per_cluster+diff].tolist()
+            else: candidates = tmp_idx[:per_cluster].tolist()
+        else:
+            logging.info(f'Cluster {i} has too few candidates: {len(tmp_idx)} found')
+            candidates = tmp_idx.tolist()
+        idx.extend(candidates)
+
+    logging.info(f'{len(idx)} total candidates found')
+    idx = np.random.choice(idx, args.candidates, replace=False)
+
+    logging.info('Retrieving Relevant IDs')
+    new_df = triples_df.loc[idx]
+
+    end = time.time()-start 
+    logging.info(f'Completed Triples collection in {end} seconds')
+
+    new_df.to_csv(args.out, sep='\t', header=False, index=False)
+    return 0
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.verbose: log_level = logging.DEBUG
+    else: log_level = logging.INFO
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=log_level)
+    logging.info('--Initialising Candidate Choice Using Cosine Ranking Sampler--')
+    main(args)
+
+
+
+
+
---
+++ scripts/metropolishastings/run_process.py
@@ -0,0 +1,121 @@
+import numpy as np 
+import pandas as pd
+import argparse
+import logging 
+import time 
+from typing import Any, NamedTuple
+
+class Config(NamedTuple):
+    triples : Any
+    k : int 
+    t : float 
+
+
+class Process:
+    state_id = 0
+    def __init__(self, config : Config) -> None:
+        self.triples = config.triples
+        self.index = np.arange(len(config.triples)) # Index for candidates
+        self.k = config.k # Num samples for mean
+        self.t = config.t # Threshold similarity
+        self.c = None # Set of Candidates
+        self.norm = lambda x : x / np.linalg.norm(x)
+
+    def _distance(self, x, mean):
+        return np.dot(self.norm(x), self.norm(mean))
+
+    def _get_indices(self): # Check we have enough candidates to sample
+        l_c = len(self.c)
+        if l_c > self.k:
+            return np.random.choice(self.c, self.k, replace=False)
+        else:
+            return self.c 
+    
+    def _get_mean(self):
+        idx = self._get_indices()
+
+        if len(idx) > 1:
+            return np.mean(self.triples[idx], axis=0) # Get random K from candidate set
+        else:
+            return self.triples[idx]
+
+    def _step(self):
+        c_id = np.random.choice(self.index) 
+        c = self.triples[c] # Get random candidate
+
+        K = self._get_mean()
+        d = self._distance(c, K) # Cosine Similarity
+
+        if d < self.t: # If candidate dissimilarity over threshold
+            self.state_id = c_id # Accept Candidate
+
+        return self.state_id
+    
+    def run(self, x0, k):
+        self.state_id = x0
+        t = 0 
+        self.c = set() # Set allows for the compiler to ignore candidates we have already accepted
+        logging.info(f'Retrieving {k} candidates with starting id: {x0}')
+        assert self.c is not None
+        self.c.add(x0)
+        start = time.time()
+        while len(self.c) < k:
+            self.c.add(self._step())
+            t += 1
+            if t % 1000: logging.info(f'{t} steps complete, {len(self.c)} candidates found')
+        end = time.time() - start 
+
+        logging.info(f'Completed collection in {end} seconds')
+
+        return np.array(list(self.c)), t
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-textsource', type=str)
+parser.add_argument('-embedsource', type=str)
+parser.add_argument('-k', type=int, default=100)
+parser.add_argument('-t', type=float, default=0.0)
+parser.add_argument('-c', type=int, default=1e5)
+parser.add_argument('-out', type=str)
+
+
+def main(args):
+    cols = ['query', 'psg+', 'psg-']
+    types = {col : str for col in cols}
+    
+    logging.info('Reading Text...')
+    triples_df = pd.read_csv(args.textsource, sep='\t', header=None, index_col=False, names=cols, dtype=types)
+
+    logging.info('Reading Embeddings...')
+    with open(args.source, 'rb') as f:
+        array = np.load(f)
+    
+    config = Config(
+        triples=array,
+        k = args.k,
+        t = args.t
+    )
+
+    model = Process(config)
+    if args.start:
+        start_id = args.start 
+    else:
+        start_id = np.random.randint(0, len(config.triples))
+
+    idx, t = model.run(start_id, args.c)
+
+    new_df = triples_df[idx]
+
+    new_df.to_csv(args.out, sep='\t', header=False, index=False)
+
+    logging.info(f'{args.c} samples found in {t} steps, Saving...')
+
+    return 0 
+
+    
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+    logging.info('--Initialising Candidate Choice Using Markov Process--')
+    main(parser.parse_args())
