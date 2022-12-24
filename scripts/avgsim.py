a042dd331d9a40bcc2cfabda2b999598de2c5a98
Andrew Parry <2393265p@student.gla.ac.uk>
2022-12-24 01:04:15 +0000

    Changed summary process

--- scripts/avgsim.py
+++ scripts/avgsim.py
@@ -18,8 +18,7 @@ def main(args):
     with open(args.embeddings, 'rb') as f:
         embed = np.load(f)
 
-    df = {'file':[], 'avg_sim':[], 'half_avg_sim':[], 'quarter_avg_sim':[], 'quarter3_avg_sim':[]}
-    cut = lambda x, y : x[:y, :y]
+    df = {'file':[], 'avg_sim':[]}
     for file in args.files:
         logging.info(f'Currently Computing Similarity for {file}')
         with open(os.path.join(args.dir, file), 'rb') as f:
@@ -27,17 +26,13 @@ def main(args):
 
         df['file'].append(file)
         idx = np.array(idx)
-        tmp_embeddings = embed[idx]
-        half = len(idx) // 2
-        quarter = len(idx) // 4
-        quarter3 = half + quarter
-        sim_matrix = cosine_similarity(tmp_embeddings)
+        tmp = embed[idx]
         
-        df['avg_sim'].append(np.mean(sim_matrix))
-        df['half_avg_sim'].append(np.mean(cut(sim_matrix, half)))
-        df['quarter_avg_sim'].append(np.mean(cut(sim_matrix, quarter)))
-        df['quarter3_avg_sim'].append(np.mean(cut(sim_matrix, quarter3)))
-
+        sim_sum = 0
+        for i in range(len(idx)):
+            sim_sum += np.sum(cosine_similarity(tmp[i].reshape(1, -1), tmp))
+        
+        df['avg_sim'].append(sim_sum / (len(idx) ** 2))
     df = pd.DataFrame(df)
     df.to_csv(args.out, index=False)
     
